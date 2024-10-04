8b853dfda0fee9b4027bcbd9d3c5a0d41c9a1bef
Patrik Gyalog <contact.jaydendev@gmail.com>
2024-10-04 14:07:41 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace villanyos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileNameBox = new System.Windows.Forms.TextBox();
+            this.fileNameBtn = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.készítőToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lineLabel = new System.Windows.Forms.Label();
+            this.tripLabel = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tripTime = new System.Windows.Forms.Label();
+            this.isShortestOrLongest = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fileNameBox
+            // 
+            this.fileNameBox.Location = new System.Drawing.Point(18, 361);
+            this.fileNameBox.Name = "fileNameBox";
+            this.fileNameBox.Size = new System.Drawing.Size(100, 20);
+            this.fileNameBox.TabIndex = 0;
+            // 
+            // fileNameBtn
+            // 
+            this.fileNameBtn.Location = new System.Drawing.Point(124, 361);
+            this.fileNameBtn.Name = "fileNameBtn";
+            this.fileNameBtn.Size = new System.Drawing.Size(75, 23);
+            this.fileNameBtn.TabIndex = 1;
+            this.fileNameBtn.Text = "Betöltés";
+            this.fileNameBtn.UseVisualStyleBackColor = true;
+            this.fileNameBtn.Click += new System.EventHandler(this.fileNameBtn_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(375, 27);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(247, 316);
+            this.listBox1.TabIndex = 2;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.készítőToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(639, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // készítőToolStripMenuItem
+            // 
+            this.készítőToolStripMenuItem.Name = "készítőToolStripMenuItem";
+            this.készítőToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.készítőToolStripMenuItem.Text = "Készítő";
+            this.készítőToolStripMenuItem.Click += new System.EventHandler(this.készítőToolStripMenuItem_Click);
+            // 
+            // lineLabel
+            // 
+            this.lineLabel.AutoSize = true;
+            this.lineLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.lineLabel.Location = new System.Drawing.Point(12, 42);
+            this.lineLabel.Name = "lineLabel";
+            this.lineLabel.Size = new System.Drawing.Size(0, 31);
+            this.lineLabel.TabIndex = 4;
+            // 
+            // tripLabel
+            // 
+            this.tripLabel.AutoSize = true;
+            this.tripLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.tripLabel.Location = new System.Drawing.Point(14, 73);
+            this.tripLabel.Name = "tripLabel";
+            this.tripLabel.Size = new System.Drawing.Size(0, 20);
+            this.tripLabel.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 151);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // tripTime
+            // 
+            this.tripTime.AutoSize = true;
+            this.tripTime.Location = new System.Drawing.Point(15, 93);
+            this.tripTime.Name = "tripTime";
+            this.tripTime.Size = new System.Drawing.Size(0, 13);
+            this.tripTime.TabIndex = 7;
+            // 
+            // isShortestOrLongest
+            // 
+            this.isShortestOrLongest.AutoSize = true;
+            this.isShortestOrLongest.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.isShortestOrLongest.Location = new System.Drawing.Point(371, 359);
+            this.isShortestOrLongest.Name = "isShortestOrLongest";
+            this.isShortestOrLongest.Size = new System.Drawing.Size(0, 20);
+            this.isShortestOrLongest.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(639, 396);
+            this.Controls.Add(this.isShortestOrLongest);
+            this.Controls.Add(this.tripTime);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.tripLabel);
+            this.Controls.Add(this.lineLabel);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.fileNameBtn);
+            this.Controls.Add(this.fileNameBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox fileNameBox;
+        private System.Windows.Forms.Button fileNameBtn;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem készítőToolStripMenuItem;
+        private System.Windows.Forms.Label lineLabel;
+        private System.Windows.Forms.Label tripLabel;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label tripTime;
+        private System.Windows.Forms.Label isShortestOrLongest;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace villanyos
+{
+    public partial class Form1 : Form
+    {
+
+        Dictionary<string, TramLine> tramLines = new Dictionary<string, TramLine>();
+        TramLine longestTramLine = new TramLine();
+        TramLine shortestTramLine = new TramLine();
+        public struct TramLine
+        {
+            public string line;
+            public List<Stop> stops;
+            public int time;
+        }
+
+        public struct Stop
+        {
+            public string name;
+            public int timeToNextStop;
+        }
+
+
+        string fileName = "./data.txt";
+        public Form1()
+        {
+            InitializeComponent();
+
+            fileNameBox.Text = fileName;
+
+            comboBox1.Visible = false;
+        }
+
+
+
+        private void fileNameBtn_Click(object sender, EventArgs e)
+        {
+            fileName = fileNameBox.Text;
+
+            readData();
+
+            getLongestAndShortestLine();
+        }
+
+        void getLongestAndShortestLine()
+        {
+            int max = 0;
+            int min = int.MaxValue;
+
+            foreach (string lineNumber in tramLines.Keys)
+            {
+                int time = 0;
+                foreach (Stop stop in tramLines[lineNumber].stops)
+                {                   
+                    time += stop.timeToNextStop;
+                }
+
+
+                if(time > max)
+                {
+                    max = time;
+                    longestTramLine.line = lineNumber;
+                }
+
+                if(time < min)
+                {
+                    min = time;
+                    shortestTramLine.line = lineNumber;
+                }
+            }
+        }
+
+        void readData()
+        {
+            string[] file = File.ReadAllLines(fileName);
+
+            foreach (string line in file)
+            {
+                string[] spl = line.Split(';');
+                string tramNumber = spl[0];
+                string stop = spl[1];
+                int timeToNext =int.Parse(spl[2]);
+
+                if (!comboBox1.Items.Contains(tramNumber))
+                {
+                    comboBox1.Items.Add(tramNumber);
+                }
+
+                TramLine tramLine = new TramLine
+                {
+                    line = tramNumber,
+                    stops = new List<Stop>(),
+                    time = 0
+                };
+                Stop tramStop = new Stop
+                {
+                    name = stop,
+                    timeToNextStop = timeToNext
+                };
+
+                tramLine.stops.Add(tramStop);
+
+                if (tramLines.ContainsKey(tramNumber))
+                {
+                    tramLines[tramNumber].stops.Add(tramStop);
+                }
+                else
+                {
+                    tramLines.Add(tramNumber, tramLine);
+                }
+
+                comboBox1.Visible = true;
+                fileNameBtn.Visible = false;
+                fileNameBox.Visible = false;
+            }
+        }
+
+        void printData(string tramNumber)
+        {
+            listBox1.Items.Clear();
+            listBox1.Items.Add(tramNumber + "-es vonal:");
+            int time = 0;
+
+            foreach (Stop stop in tramLines[tramNumber].stops)
+            {
+                Console.WriteLine(time);
+                lineLabel.Text = tramNumber;
+                tripLabel.Text = tramLines[tramNumber].stops[0].name + " - " + tramLines[tramNumber].stops[tramLines[tramNumber].stops.Count -1].name;
+
+
+                listBox1.Items.Add(stop.name + " - " + time + "'");
+                time+=stop.timeToNextStop;
+                Console.WriteLine(time);
+            }
+
+            tripTime.Text = "Menetidő: " + (time) + " perc";
+
+            if(longestTramLine.line != tramNumber && shortestTramLine.line != tramNumber)
+            {
+                isShortestOrLongest.Text = "";
+            }
+
+            if(longestTramLine.line == tramNumber)
+            {
+                isShortestOrLongest.Text = "Ez a leghosszabb villamosvonal";
+            }
+            if(shortestTramLine.line == tramNumber)
+            {
+                isShortestOrLongest.Text = "Ez a legrövidebb villamosvonal";
+            } 
+        }
+
+        private void készítőToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Gyalog Patrik");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string tramNumber = comboBox1.SelectedItem.ToString();
+            Console.Write(tramNumber);
+            printData(tramNumber);
+        }
+    }
+}
